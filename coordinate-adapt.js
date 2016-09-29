e82df9dbdd517249f271ba1caff999474d02956f
charlton_chang <charlton_chang@gemteks.com>
2016-09-29 11:05:29 +0800

    [coordinate-adapt v1.1.4] Enable to support indoor positioning.

--- coordinate-adapt.js
+++ coordinate-adapt.js
@@ -29,7 +29,33 @@ exports.InitLF_db = function InitLF_db(configs) {
     // buckets.LF = cluster.openBucket(args.bucketname, args.pw);
     console.log(configs);
     elasticObj.client = elasticsearch.Client(configs);
-    fingerprint.setupDB(configs.host, configs.index);
+    elasticObj.index = configs.index;
+
+    elasticObj.client.ping({
+        requestTimeout: 3000,
+        hello: 'elasticsearch!'
+    }, function (err) {
+        if (err) {
+            return;
+        }
+
+        console.log('Elasticsearch cluster is OK !');
+
+        fingerprint.setupDB(elasticObj.client, configs.index);
+
+        //for indoor fingerprint demo
+        var gwList = [
+            '00001c497b48dcdd', '00001c497b48dce6',
+            '00001c497b48dbcf', '00001c497b48dbd5',
+            '00001c497b48dbc0', '00001c497b48dbc1',
+            '00001c497b48dbf7', '00001c497b48dbfc',
+            '00001c497b48dc3e', '00001c497b48db93',
+            '00001c497b48dbed', '00001c497b48dbdd'
+        ];
+
+        setFingerprintFilter('gps-history_gemtek_8F', gwList, function(err, res) {
+        });
+    });
 }
 
 //initial station Info db
@@ -70,9 +96,36 @@ exports.disconnectBase_db = function disconnectBase_db() {
 // }
 
 // Saves effect GPS coordinate of NODE
-exports.NodeGPSInsert = function NodeGPSInsert(nodeGroup, callback) {
-    console.log('Gateway count', nodeGroup.Gateway.length);
-    recordFingerprint(nodeGroup, function(err, res) {
+exports.NodeGPSInsert = function NodeGPSInsert(object, callback) {
+//    console.log('Gateway count', nodeGroup.Gateway.length);
+
+    if (!object || !object.nodeGPS_N || !object.nodeGPS_E || !object.Gateway || object.Gateway.length == 0) {
+        return callback(new Error('Invalid fingerprint data !'));
+    }
+
+    //discard redundant data
+    var dataArray = object.Gateway;
+    var idArray = [];
+    var validDataArray = [];
+
+    for (var idx in dataArray) {
+        if (idArray.indexOf(dataArray[idx].gatewayID) != -1) {
+            continue;
+        }
+
+        idArray.push(dataArray[idx].gatewayID);
+        validDataArray.push(dataArray[idx]);
+    }
+
+    var tbID = null;
+
+    //for indoor fingerprint demo
+    if (getNodeST(object.nodeDATA, 6) == 1) {
+        tbID = 'gps-history_gemtek_8F';
+    }
+
+    //record fingerprint data
+    recordFingerprint(tbID, {GPS_N: object.nodeGPS_N, GPS_E: object.nodeGPS_E}, validDataArray, function(err, res) {
         if (!callback) {
             return;
         }
@@ -86,36 +139,48 @@ exports.NodeGPSInsert = function NodeGPSInsert(nodeGroup, callback) {
 }
 
 //coordinate position transfer
-exports.CoorTrans = function CoorTrans(station, callback) {
-    console.log('coordinate-adapt ver. ', pjson.version);
+exports.CoorTrans = function CoorTrans(object, callback) {
+//    console.log('coordinate-adapt ver. ', pjson.version);
 
-//     for (var i = 0; i < station.length; i++) {
-//         console.log(station[i]);
+    var dataArray = object.Gateway;
+
+//     for (var i = 0; i < dataArray.length; i++) {
+//         console.log(dataArray[i]);
 //     }
 
     //discard redundant data
     var idArray = [];
     var staData = [];
 
-    for (var idx in station) {
-        if (idArray.indexOf(station[idx].GWID) != -1) {
+    for (var idx in dataArray) {
+        if (idArray.indexOf(dataArray[idx].gatewayID) != -1) {
             continue;
         }
 
-        idArray.push(station[idx].GWID);
-        staData.push(station[idx]);
+        idArray.push(dataArray[idx].gatewayID);
+        staData.push(dataArray[idx]);
     }
 
-    //try to find finger print
-    findFingerprint(staData, function(err, result) {
+    //try to find fingerprint
+    var tbID = null;
+
+    //for indoor fingerprint demo
+    if (getNodeST(object.nodeDATA, 6) == 1) {
+        tbID = 'gps-history_gemtek_8F';
+    }
+
+    findFingerprint(tbID, staData, function(err, result) {
+
         if (err) {
             //console.log('Find fingerprint failed:\n' + err);
 
+            if (getNodeST(object.nodeDATA, 6) == 1) {
+                return callback(new Error('Query coordinate failed !'));
+            }
+
             //get station information for location estimation
             getStationInfo(idArray, function(err, res) {
-                if (err) {
-                    console.log('Get station information failed:');
-                    console.log(err);
+                if (err && (!res || res == {})) {
                     return callback(err);
                 }
 
@@ -126,7 +191,7 @@ exports.CoorTrans = function CoorTrans(station, callback) {
                 for (var i = 0; i < staData.length; i++) {
                     var circle = {x: 0, y: 0};
                     var data = staData[i];
-                    var info = res[data.GWID];
+                    var info = res[data.gatewayID];
 
                     if (info) {
                         var coordinate = {GpsX: parseFloat(info.GpsX), GpsY: parseFloat(info.GpsY)};
@@ -137,7 +202,7 @@ exports.CoorTrans = function CoorTrans(station, callback) {
                             circle = geoUtil.convertGPSToCartesian(coordinate, base);
                         }
 
-                        var signal = Math.round(parseFloat(data.RSSI) + parseFloat(data.SNR) / 10);
+                        var signal = Math.round(data.rssi + data.snr / 10);
                         circle.r = trilateration.countDistanceByRSSI(signal);
                         circles.push(circle);
                     }
@@ -189,48 +254,127 @@ exports.CoorTrans = function CoorTrans(station, callback) {
 //    })
 //}
 
-//find fingerprint with input dataArray: [{GWID, RSSI, SNR}], output callback(err, result)
-function findFingerprint(dataArray, callback) {
-    var sigArray = dataArray.map(function(item) {
-        var signal = Math.round(parseInt(item.RSSI) + parseFloat(item.SNR) / 10);
-        return {GWID: item.GWID, signal: signal};
-    });
+//set fingerprint filter with tbID, gwidList: [GWID], output callback(err, res)
+function setFingerprintFilter(tbID, gwidList, callback) {
+    var docID = tbID? tbID: 'gps-history';
 
-    fingerprint.find(sigArray, function(err, res) {
+    // Index document
+    elasticObj.client.index({
+        index: elasticObj.index,
+        type: 'gateway-filter',
+        id: docID,
+        body: {
+            validList: gwidList
+        }
+    }, function (err, res) {
         if (err) {
             return callback(err);
         }
 
-        callback(null, res);
+        var result = {created: res.created};
+        callback(null, result);
     });
 }
 
-//record fingerprint with input fpData: {nodeGPS_N, nodeGPS_E, Gateway}, output callback(err, result)
-//Gateway: [{rssi, snr, time, gatewayID, mac}]
-function recordFingerprint(fpData, callback) {
-    if (!fpData || !fpData.nodeGPS_N || !fpData.nodeGPS_E || !fpData.Gateway || fpData.Gateway.length == 0) {
-        callback(new Error('Invalid fingerprint data !'));
-        return;
-    }
+//get fingerprint filter with tbID, output callback(err, res)
+function getFingerprintFilter(tbID, callback) {
+    var docID = tbID? tbID: 'gps-history';
 
-    var pos = {GPS_N: fpData.nodeGPS_N, GPS_E: fpData.nodeGPS_E};
-    var sigData = fpData.Gateway.map(function(item) {
-        var signal = Math.round(item.rssi + item.snr / 10);
-        return {GWID: item.gatewayID, signal: signal, time: item.time};
-    });
-
-    fingerprint.record(pos, sigData, function(err, res) {
+    elasticObj.client.get({
+        index: elasticObj.index,
+        type: 'gateway-filter',
+        id: docID
+    }, function (err, res) {
         if (err) {
-            callback(err);
-            return;
+            return callback(err);
         }
 
-        callback(null, res);
+        var result = {validList: res._source.validList};
+        callback(null, result);
+    });
+}
+
+//find fingerprint with input dataArray: [{gatewayID, rssi, snr, time, mac}], output callback(err, result)
+function findFingerprint(tbID, dataArray, callback) {
+    var table = tbID? tbID: 'gps-history';
+
+    getFingerprintFilter(table, function(err, res) {
+        var validDataArray = [];
+
+        if (!err) {
+            var validList = res.validList;
+
+            for (var idx in dataArray) {
+                var gwid = dataArray[idx].gatewayID;
+
+                if (validList.indexOf(gwid) != -1) {
+                    validDataArray.push(dataArray[idx]);
+                }
+            }
+        } else {
+            validDataArray = dataArray;
+        }
+
+        var sigArray = validDataArray.map(function(item) {
+            var signal = Math.round(item.rssi + item.snr / 10);
+            return {GWID: item.gatewayID, signal: signal};
+        });
+
+        fingerprint.find(table, sigArray, function(err, res) {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, res);
+        });
+    });
+}
+
+//record fingerprint with input tbID, position: {GPS_N, GPS_E}, dataArray: [{gatewayID, rssi, snr, time, mac}], output callback(err, result)
+function recordFingerprint(tbID, position, dataArray, callback) {
+    var table = tbID? tbID: 'gps-history';
+
+    getFingerprintFilter(table, function(err, res) {
+        var validDataArray = [];
+
+        if (!err) {
+            var validList = res.validList;
+
+            for (var idx in dataArray) {
+                var gwid = dataArray[idx].gatewayID;
+
+                if (validList.indexOf(gwid) != -1) {
+                    validDataArray.push(dataArray[idx]);
+                }
+            }
+        } else {
+            validDataArray = dataArray;
+        }
+
+        if (validDataArray.length == 0) {
+            return callback(new Error('Invalid fingerprint data !'));
+        }
+
+        var sigData = validDataArray.map(function(item) {
+            var signal = Math.round(item.rssi + item.snr / 10);
+            return {GWID: item.gatewayID, signal: signal, time: item.time};
+        });
+
+        //record fingerprint data
+        fingerprint.record(table, position, sigData, function(err, res) {
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            callback(null, res);
+        });
     });
 }
 
 //get station information with input dataArray: [GWID], output callback(err, result)
 function getStationInfo(dataArray, callback) {
+
     if (!buckets.Base) {
         return callback(new Error('Database is not set !'));
     }
@@ -246,19 +390,19 @@ function getStationInfo(dataArray, callback) {
         var failCase = '';
 
         for (var i = 0; i < dataArray.length; i++) {
-            var gwid = dataArray[i];
+            var gwid = dataArray[i].substring(4).toUpperCase();
             var info = infoList[gwid];
 
             if (!info) {
-                failCase += (' ' +  gwid);
+                failCase += (' ' +  dataArray[i]);
                 continue;
             }
 
-            tmpResult[gwid] = info;
+            tmpResult[dataArray[i]] = info;
         }
 
-        if (Object.keys(tmpResult).length < dataArray.length && failCase != '') {
-            callback(new Error('Get station information for' + failCase + ' failed !'));
+        if (failCase != '') {
+            callback(new Error('Get station information for' + failCase + ' failed !'), tmpResult);
             return;
         }
 
